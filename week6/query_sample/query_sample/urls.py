b782ba88161ac0732fb307ba93d54b0e2a542772
AmirabbasAfrasiyabi <aa.afrasiyabi@yahoo.com>
2025-07-04 16:02:37 +0330

    time 1 ended (work with type of queryset and lookup)

--- week6/query_sample/query_sample/urls.py
+++ week6/query_sample/query_sample/urls.py
@@ -16,7 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from posts.views import (retrive_posts ,
+                retrieve_posts_exclude_sample)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('posts/', retrive_posts),
+    path('posts/exclude' , retrieve_posts_exclude_sample)
 ]
