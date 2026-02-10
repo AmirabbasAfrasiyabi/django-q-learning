46075fcaa56f0d669f948a1011a22dc45fb29b1c
AmirabbasAfrasiyabi <aa.afrasiyabi@yahoo.com>
2026-02-10 07:11:37 +0330

    delete mektab course

--- MaktabKhone/begginer/website/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from website.views import home, about , contact
-
-
-urlpatterns = [
-    path('' , home , name = 'index'),
-    path('about/', about ,name = 'about'),
-    path('contact/', contact ,name = 'contact'),
-]
